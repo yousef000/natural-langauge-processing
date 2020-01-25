85ba75b790845f520ef492b9eec17475a71bcf45
yousef000 <ydost@ucsc.edu>
2020-01-25 15:20:23 -0800

    trigram_count working but trigram model not

--- language-modeling-and-smoothing/language-modeling-and-smoothing.py
+++ language-modeling-and-smoothing/language-modeling-and-smoothing.py
@@ -79,7 +79,61 @@ def get_bi_model(bigram_count):
             }]
     return bigram_model
 
+def get_tri_count(test_data):
+    ## compute the count of trigram C(x,y,z)
+    trigram_count = {}
+    for sentence in test_data:
+        words = sentence.split()
+        for i in range(len(words)):
+            ## if word is unknown then change token to UNK
+            if words[i] not in freqs:
+                words[i] = "UNK"
+            if i != len(words)-1 and words[i+1] not in freqs:
+                words[i+1] = "UNK"
+            if i != len(words)-2 and i != len(words)-1 and words[i+2] not in freqs:
+                words[i+2] = "UNK"
+            
+            ## if last word
+            if i == len(words)-1:
+                break
+            if i == len(words)-2:
+                if (words[i+1], "<STOP>") in trigram_count:
+                    trigram_count[(words[i], words[i+1], "<STOP>")] += 1
+                else:
+                    trigram_count[(words[i], words[i+1], "<STOP>")] = 1
+                    
+            elif (words[i], words[i+1], words[i+2]) in trigram_count:
+                trigram_count[(words[i], words[i+1], words[i+2])] += 1
+                
+            else:
+                trigram_count[(words[i], words[i+1], words[i+2])] = 1
+    return trigram_count
 
+def get_tri_model(trigram_count):
+    trigram_model = {}
+    probability = 0.0
+    for key in trigram_count:
+
+        # To compute trigram probability of a word z (key[2]) given
+        # previous word x (key[0]) and y (key[1]), divide the count of trigram
+        # C(x,y,z) and sum of all trigrams that share the same first word x
+        # and y which means bigram_count of x and y
+
+        # whether word is OOV or not
+        if key[0] in freqs:
+            probability = bigram_count[key]/float(freqs[key[0]])
+        else:
+            probability = bigram_count[key]/float(freqs["UNK"])
+        
+        if key[0] in bigram_model:
+            bigram_model[key[0]].append({
+                key[1]: probability
+            })
+        else:
+            bigram_model[key[0]] = [{
+                key[1]: probability
+            }]
+    return bigram_model
 if __name__ == '__main__':
 
     with open('data/1b_benchmark.train.tokens') as my_file:
@@ -92,13 +146,15 @@ if __name__ == '__main__':
     get_freq(freqs, test_data)
     print('len freqs\n', len(freqs))
 
-    bigram_count = get_bi_count(test_data)
+    # bigram_count = get_bi_count(test_data)
     # print('bigram count\n', bigram_count)
 
-    bigram_model = get_bi_model(bigram_count)
+    # bigram_model = get_bi_model(bigram_count)
 
     # print("bigram model: \n", bigram_model)
 
+    trigram_count = get_tri_count(test_data)
+    print(trigram_count)
     total = 0
     for i in bigram_model:
         for j in bigram_model[i]:
